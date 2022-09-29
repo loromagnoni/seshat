807d8f4ac8d7aff063b9a64f6a970808968ade29
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2022-09-29 22:39:34 +0200

    feat: add auth page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+import * as firebaseAuth from '@firebase/auth'
 import * as theme from 'config/chakra.config'
+import { initializeApp, getApp, getApps } from 'firebase/app'
 
+import { AuthProvider } from '@redwoodjs/auth'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -12,15 +15,42 @@ import './index.css'
 
 const extendedTheme = extendTheme(theme)
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+
+  /** Optional config, may be needed, depending on how you use firebase
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  **/
+}
+
+const firebaseApp = ((config) => {
+  const apps = getApps()
+  if (!apps.length) {
+    initializeApp(config)
+  }
+  return getApp()
+})(firebaseConfig)
+
+export const firebaseClient = {
+  firebaseAuth,
+  firebaseApp, // optional
+}
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <ColorModeScript />
-      <ChakraProvider theme={extendedTheme}>
-        <RedwoodApolloProvider>
-          <Routes />
-        </RedwoodApolloProvider>
-      </ChakraProvider>
+      <AuthProvider client={firebaseClient} type="firebase">
+        <ColorModeScript />
+        <ChakraProvider theme={extendedTheme}>
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </ChakraProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,32 +7,36 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route, Private } from '@redwoodjs/router'
 
 import TransactionCategoriesLayout from 'src/layouts/TransactionCategoriesLayout'
 import TransactionsLayout from 'src/layouts/TransactionsLayout'
 
 import HomeLayout from './layouts/HomeLayout/HomeLayout'
+import AuthPage from './pages/AuthPage/AuthPage'
 
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={HomeLayout}>
-        <Set wrap={TransactionCategoriesLayout}>
-          <Route path="/transaction-categories/new" page={TransactionCategoryNewTransactionCategoryPage} name="newTransactionCategory" />
-          <Route path="/transaction-categories/{id:Int}/edit" page={TransactionCategoryEditTransactionCategoryPage} name="editTransactionCategory" />
-          <Route path="/transaction-categories/{id:Int}" page={TransactionCategoryTransactionCategoryPage} name="transactionCategory" />
-          <Route path="/transaction-categories" page={TransactionCategoryTransactionCategoriesPage} name="transactionCategories" />
+      <Private unauthenticated="auth">
+        <Set wrap={HomeLayout}>
+          <Set wrap={TransactionCategoriesLayout}>
+            <Route path="/transaction-categories/new" page={TransactionCategoryNewTransactionCategoryPage} name="newTransactionCategory" />
+            <Route path="/transaction-categories/{id:Int}/edit" page={TransactionCategoryEditTransactionCategoryPage} name="editTransactionCategory" />
+            <Route path="/transaction-categories/{id:Int}" page={TransactionCategoryTransactionCategoryPage} name="transactionCategory" />
+            <Route path="/transaction-categories" page={TransactionCategoryTransactionCategoriesPage} name="transactionCategories" />
+          </Set>
+          <Route path="/" page={HomePage} name="home" />
+          <Set wrap={TransactionsLayout}>
+            <Route path="/transactions/new" page={TransactionNewTransactionPage} name="newTransaction" />
+            <Route path="/transactions/{id:Int}/edit" page={TransactionEditTransactionPage} name="editTransaction" />
+            <Route path="/transactions/{id:Int}" page={TransactionTransactionPage} name="transaction" />
+            <Route path="/transactions" page={TransactionTransactionsPage} name="transactions" />
+          </Set>
+          <Route notfound page={NotFoundPage} />
         </Set>
-        <Route path="/" page={HomePage} name="home" />
-        <Set wrap={TransactionsLayout}>
-          <Route path="/transactions/new" page={TransactionNewTransactionPage} name="newTransaction" />
-          <Route path="/transactions/{id:Int}/edit" page={TransactionEditTransactionPage} name="editTransaction" />
-          <Route path="/transactions/{id:Int}" page={TransactionTransactionPage} name="transaction" />
-          <Route path="/transactions" page={TransactionTransactionsPage} name="transactions" />
-        </Set>
-        <Route notfound page={NotFoundPage} />
-      </Set>
+      </Private>
+      <Route path="/auth" page={AuthPage} name="auth" />
     </Router>
   )
 }
