37497e9dc35f917a36e06a13b1e17c703dd7d226
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2022-09-29 22:50:04 +0200

    feat: add google auth
    ff

--- web/src/pages/AuthPage/AuthPage.tsx
+++ web/src/pages/AuthPage/AuthPage.tsx
@@ -1,20 +1,44 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import {
+  Flex,
+  Heading,
+  IconButton,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { FcGoogle } from 'react-icons/fc'
+
+import { useAuth } from '@redwoodjs/auth'
+import { navigate, routes } from '@redwoodjs/router'
 
 const AuthPage = () => {
-  return (
-    <>
-      <MetaTags title="Auth" description="Auth page" />
+  const { logIn } = useAuth()
 
-      <h1>AuthPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AuthPage/AuthPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>auth</code>, link to me with `
-        <Link to={routes.auth()}>Auth</Link>`
-      </p>
-    </>
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy Seshat cool features ✌️
+          </Text>
+        </Stack>
+
+        <IconButton
+          aria-label="google-auth"
+          shadow={'lg'}
+          color={'white'}
+          onClick={() => logIn().then(() => navigate(routes.home()))}
+        >
+          <FcGoogle size={20} />
+        </IconButton>
+      </Stack>
+    </Flex>
   )
 }
 
