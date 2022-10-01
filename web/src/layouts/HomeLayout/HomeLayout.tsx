ea210014965edcf68efa8d227656bb2e73087ef5
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2022-10-02 00:14:49 +0200

    feat: display google account info

--- web/src/layouts/HomeLayout/HomeLayout.tsx
+++ web/src/layouts/HomeLayout/HomeLayout.tsx
@@ -154,7 +154,7 @@ interface MobileProps extends FlexProps {
   onOpen: () => void
 }
 const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  const { logOut } = useAuth()
+  const { logOut, currentUser } = useAuth()
   return (
     <Flex
       ml={{ base: 0, md: 60 }}
@@ -193,22 +193,14 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
               _focus={{ boxShadow: 'none' }}
             >
               <HStack>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
+                <Avatar size={'sm'} src={currentUser.picture} />
                 <VStack
                   display={{ base: 'none', md: 'flex' }}
                   alignItems="flex-start"
                   spacing="1px"
                   ml="2"
                 >
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
+                  <Text fontSize="sm">{currentUser.email}</Text>
                 </VStack>
                 <Box display={{ base: 'none', md: 'flex' }}>
                   <FiChevronDown />
